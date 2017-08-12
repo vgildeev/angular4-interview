b8b0a96b7aeac1b6d2763dccc6248b7af4384766
Slavik Gildeev <someheal@gmail.com>
2017-08-12 23:22:33 +0300

    Added markup for profile component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 import { ProfilesService } from '../../shared/services';
 import { Profile } from '../../shared/models';
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+
+import { ProfilesService } from '../../shared/services';
+import { Profile } from '../../shared/models';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  profile: Profile;
 
-  constructor() { }
+  constructor(
+    private profilesService: ProfilesService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params =>
+      this.profilesService.getProfileById(params.id)
+        .then((profile: Profile) => this.profile = profile)
+        .catch(err => console.log(err))
+    );
   }
 
 }
