38a678df3d7c8333fbac85edf86cc1c031f5af48
Slavik Gildeev <someheal@gmail.com>
2017-08-12 21:49:32 +0300

    Added some profiles mocks

---
+++ src/shared/mocks/profiles.mock.ts
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: '1',
+    nickname: 'Bober',
+    avatar: 'http://placehold.it/400x400',
+    bio: 'Bober`s bio',
+    repositories: [],
+  },
+  {
+    id: '2',
+    nickname: 'Daggit',
+    avatar: 'http://placehold.it/400x400',
+    bio: 'Daggit`s bio',
+    repositories: [],
+  },
+  {
+    id: '3',
+    nickname: 'Norbert',
+    avatar: 'http://placehold.it/400x400',
+    bio: 'Norbert`s bio',
+    repositories: [],
+  },
+];
