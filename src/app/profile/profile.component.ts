05dceaf2b7b3307429a552e4e4bf32ed4c8ed56c
Slavik Gildeev <someheal@gmail.com>
2017-08-13 18:07:11 +0300

    Added pagination for profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,6 +12,11 @@ import { Profile } from '../../shared/models';
 })
 export class ProfileComponent implements OnInit {
   profile: Profile;
+  perPage: number = 10;
+  paginateFrom: number = 0;
+  paginateTo: number = this.perPage;
+  currentPage: number = 1;
+  totalPages: number;
 
   constructor(
     private profilesService: ProfilesService,
@@ -22,9 +27,27 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(params =>
       this.profilesService.getProfileById(params.id)
-        .then((profile: Profile) => this.profile = profile)
+        .then((profile: Profile) => {
+          this.profile = profile;
+          this.totalPages = Math.ceil(profile.repositories.length / this.perPage);
+        })
         .catch(err => this.router.navigate(['/home']))
     );
   }
 
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage += 1;
+      this.paginateFrom = (this.currentPage - 1) * this.perPage;
+      this.paginateTo = this.currentPage * this.perPage;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage -= 1;
+      this.paginateFrom = (this.currentPage - 1) * this.perPage;
+      this.paginateTo = this.currentPage * this.perPage;
+    }
+  }
 }
