5499a8cfb8e2f7193ed9418166098b8394792c48
Slavik Gildeev <someheal@gmail.com>
2017-08-13 20:01:13 +0300

    Added nicknames list to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ProfilesMocks from 'shared/mocks/profiles.mock';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  nicknames: string[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.nicknames = ProfilesMocks.map(p => p.nickname);
   }
-
 }
